54dbcb41e88beef330d75b03f37774a8d6e7bf64
Chris Rahn <rahnshoe@mail.com>
2019-10-07 14:26:55 -0700

    Convert to MVC design pattern

---
+++ Todoey/Data Model/Item.swift
@@ -0,0 +1,15 @@
+//
+//  Item.swift
+//  Todoey
+//
+//  Created by chris rahn on 10/7/19.
+//  Copyright © 2019 chris rahn. All rights reserved.
+//
+
+import Foundation
+
+class Item {
+    var title: String = ""
+    var done: Bool = false
+    
+}
